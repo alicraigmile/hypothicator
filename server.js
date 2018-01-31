a4ff308c2f9339b33675fb1b0faecd39c62861c3
Ali Craigmile <ali.craigmile@bbc.co.uk>
2018-01-31 13:26:16 +0000

    security - redirect to https if not running on localhost

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const package = require('./package')
 const port = process.env.PORT || 5000
 const app = express()
 
+// set up a route to redirect http to https
+app.enable('trust proxy');
+app.use(function(req, res, next) {
+    if (req.secure){
+        return next();
+    }
+    if  (req.headers.host.match(/^localhost[:$]/)) {
+        return next();
+    }
+    console.log(req.headers.host);
+    res.redirect("https://" + req.headers.host + req.url);
+});
+
 app.get('/status', (req, res) => res.json({version: package.version}))
 app.use(express.static('public'))
 app.use('/js/jquery', express.static('node_modules/jquery/dist/'))
