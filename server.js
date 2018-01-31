4aba25c725f1eb15680ff42d4900c6c52117ecc9
Ali Craigmile <ali.craigmile@bbc.co.uk>
2018-01-31 14:12:54 +0000

    redirect to secure host (unless running locally)

--- server.js
+++ server.js
@@ -3,18 +3,21 @@ const package = require('./package')
 const port = process.env.PORT || 5000
 const app = express()
 
+const isLocalhost =  function(host) {
+    return host.match(/^localhost[:$]/) || 
+           host.match(/^127\.0\.0\.1[:$]/);
+}
+
 // set up a route to redirect http to https
 app.enable('trust proxy');
 app.use(function(req, res, next) {
-    if (req.secure){
-        return next();
+    var noNeedToRedirect = req.secure || isLocalhost(req.headers.host)
+
+    if (noNeedToRedirect){
+        return next()
     }
-    if  (req.headers.host.match(/^localhost[:$]/)) {
-        return next();
-    }
-    console.log(req.headers.host);
-    res.redirect("https://" + req.headers.host + req.url);
-});
+    res.redirect("https://" + req.headers.host + req.url)
+})
 
 app.get('/status', (req, res) => res.json({version: package.version}))
 app.use(express.static('public'))
